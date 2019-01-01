756700209538f3934d760cb05a31d8460c42835a
Lily Li <23038037+mouyun-lily-li@users.noreply.github.com>
2018-12-31 21:49:11 -0700

    updated code

--- map_block_groups_to_wards.py
+++ map_block_groups_to_wards.py
@@ -37,32 +37,31 @@ def map_block_groups_to_wards():
         wards[ward_id] = ward_geom
 
     # read in il block groups shapefile, map block groups to wards
-    il_block_groups = fiona.open("data/block_group_shapefiles/tl_2017_17_bg.shp")
-    # il_block_groups.schema
+    with fiona.open("data/block_group_shapefiles/tl_2017_17_bg.shp") as il_block_groups:
+        # il_block_groups.schema
+        for bg in il_block_groups:
+            if bg['properties']['COUNTYFP'] == COOK_IL_COUNTYFP:
+                bg_geom_1 = shape(bg['geometry'])
+                bg_geom = ops.transform(
+                    partial(
+                        pyproj.transform,
+                        pyproj.Proj(init = 'EPSG:4326'),
+                        pyproj.Proj(
+                            proj = 'aea',
+                            lat1 = bg_geom_1.bounds[1],
+                            lat2 = bg_geom_1.bounds[3])),
+                    bg_geom_1)
+                
+                intersecting_wards = []
+                # a bg may intersect w/ multiple wards due to imperfect boundaries,
+                # so find all intersections and sort by largest % of bg
+                for ward_id, ward_geom in wards.items():
+                    if bg_geom.intersects(ward_geom):
+                        intersecting_wards.append((ward_id, bg_geom.intersection(ward_geom).area / bg_geom.area * 100))
 
-    for bg in il_block_groups:
-        if bg['properties']['COUNTYFP'] == COOK_IL_COUNTYFP:
-            bg_geom_1 = shape(bg['geometry'])
-            bg_geom = ops.transform(
-                partial(
-                    pyproj.transform,
-                    pyproj.Proj(init = 'EPSG:4326'),
-                    pyproj.Proj(
-                        proj = 'aea',
-                        lat1 = bg_geom_1.bounds[1],
-                        lat2 = bg_geom_1.bounds[3])),
-                bg_geom_1)
-            
-            intersecting_wards = []
-            # a bg may intersect w/ multiple wards due to imperfect boundaries,
-            # so find all intersections and sort by largest % of bg
-            for ward_id, ward_geom in wards.items():
-                if bg_geom.intersects(ward_geom):
-                    intersecting_wards.append((ward_id, bg_geom.intersection(ward_geom).area / bg_geom.area * 100))
-
-            if len(intersecting_wards) > 0:
-                intersecting_wards = sorted(intersecting_wards, key = lambda x: x[1], reverse = True)
-                bg_to_ward_mapping[bg['properties']['GEOID']] = intersecting_wards[0][0]
+                if len(intersecting_wards) > 0:
+                    intersecting_wards = sorted(intersecting_wards, key = lambda x: x[1], reverse = True)
+                    bg_to_ward_mapping[bg['properties']['GEOID']] = intersecting_wards[0][0]
 
     return bg_to_ward_mapping
 
